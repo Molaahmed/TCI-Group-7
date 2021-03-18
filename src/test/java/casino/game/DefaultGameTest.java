5c0f61258134eb6b7cd297140006ef53c203cfe6
Redzhep <redjep.molaahmed.rm@gmail.com>
2021-03-18 14:24:12 +0100

    Test CI action with fail test

--- src/test/java/casino/game/DefaultGameTest.java
+++ src/test/java/casino/game/DefaultGameTest.java
@@ -1,7 +1,14 @@
 package casino.game;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultGameTest {
 
+
+    @Test
+    public void testThatFail(){
+        assertEquals(true, false);
+    }
 }
